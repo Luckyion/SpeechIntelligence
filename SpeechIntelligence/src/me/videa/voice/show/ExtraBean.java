fd919aaa99e22e71c6e69f7cf1105deae4f66dd4
Luckyion <luckyion@126.com>
2016-01-04 19:49:57 +0800

    Date time receiver.

--- SpeechIntelligence/src/me/videa/voice/show/ExtraBean.java
+++ SpeechIntelligence/src/me/videa/voice/show/ExtraBean.java
@@ -1,10 +1,21 @@
 ﻿package me.videa.voice.show;
 
-public class ExtraBean {
+import java.io.Serializable;
+
+public class ExtraBean implements Serializable{
 	
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1996439640354076287L;
+
 	private int singleStrength;
 	
 	private String ip;
+	
+	private int batteryLevel;
+	
+	private TimeBean timeBean;
 
 	public int getSingleStrength() {
 		return singleStrength;
@@ -20,6 +31,22 @@ public class ExtraBean {
 
 	public void setIp(String ip) {
 		this.ip = ip;
+	}
+
+	public int getBatteryLevel() {
+		return batteryLevel;
+	}
+
+	public void setBatteryLevel(int batteryLevel) {
+		this.batteryLevel = batteryLevel;
+	}
+
+	public TimeBean getTimeBean() {
+		return timeBean;
+	}
+
+	public void setTimeBean(TimeBean timeBean) {
+		this.timeBean = timeBean;
 	}
 	
 	
--- SpeechIntelligence/src/me/videa/voice/show/HandlerWhat.java
+++ SpeechIntelligence/src/me/videa/voice/show/HandlerWhat.java
@@ -3,6 +3,8 @@
 public interface HandlerWhat {
 	
 	public static int NET_STATE = 0;
+	public static int BATTERY_STATE = 1;
+	public static int DATE_STATE = 3;
 
 }
 
